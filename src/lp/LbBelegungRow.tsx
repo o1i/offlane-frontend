5fefd7d427785bd642217dcc64ae89f8a47f7120
oliver <oliver.burkhard@gmail.com>
2020-05-25 06:43:50 +0200

    Add sus to storybook

--- src/lp/LbBelegungRow.tsx
+++ src/lp/LbBelegungRow.tsx
@@ -5,6 +5,13 @@ import { LbBelegung } from "./LbBelegung";
 import Paper from '@material-ui/core/Paper';
 import Chip from '@material-ui/core/Chip';
 import Grid from '@material-ui/core/Grid';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const useStyles = makeStyles({
     gridRow: {
@@ -22,6 +29,21 @@ export const LbBelegungRow = ({lbBelegung}: {lbBelegung: {lbInstance: LbInstance
             return(
                 <Grid item md={4}>
                     <LbBelegung lb={lbInstance} sus={sus}/>
+                    <List dense={true}>
+                    {
+                    sus.map(({name, id}) => {
+                        return(
+                            <ListItem>
+                                <ListItemText primary={name}/>
+                                <ListItemSecondaryAction>
+                                <IconButton edge="end" aria-label="delete">
+                                    <DeleteIcon />
+                                </IconButton>
+                                </ListItemSecondaryAction>
+                            </ListItem>
+                        );
+                    })}
+                    </List>
                 </Grid>
             );
         })}
