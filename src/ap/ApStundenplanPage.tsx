ea04c161d395e4dd516da00c4c7053c017267a9c
oliver <oliver.burkhard@gmail.com>
2020-06-05 07:24:42 +0200

    fix AP flexboxes

--- src/ap/ApStundenplanPage.tsx
+++ src/ap/ApStundenplanPage.tsx
@@ -4,6 +4,7 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -13,7 +14,18 @@ const useStyles = makeStyles((theme: Theme) =>
             color: theme.palette.text.secondary,
         },
         main:{
-            "background-color": "#f9f9f9"
+            "background-color": "#f9f9f9",
+        },
+        spaltenTitel: {
+            fontSize: 21,
+            "font-family": "helvetica",
+            "font-weight": "bold",
+            "color": "#666677",
+            "padding-bottom": 15,
+        },
+        form: {
+            "background-color": "#fff",
+            width: "100%"
         },
 }));
 
@@ -27,50 +39,53 @@ export const ApStundenplanPage = () => {
 
     
     return (
-        <Grid container spacing={2} direction="row" justify="flex-start" align-items="stretch" wrap="nowrap" className={classes.main}>
+        <Grid container spacing={2} direction="row" justify="flex-start" align-items="flex-start" wrap="nowrap" className={classes.main}>
 
             <Grid item container spacing={0} direction="column" md={3}>
-                <Grid item md={12}>
+                <Grid item >
                     <Typography>
-                        <Box>
+                        <Box className={classes.spaltenTitel}>
                             Lernbüro-Gruppen
                         </Box>
                     </Typography>
                 </Grid>
-                <Grid item md={12}>
-                    <Paper className={classes.paper}>item with siginuiet ndiaten idane ditan editane dtiaun edain edinaudeneeeeeee eeeeeeeeeeeeee eeeeeeeeeeeeee eeeeeeeeeeee</Paper>
+                <Grid item >
+                    <Paper className={classes.paper}>item with siginuiet ndiaten idane ditan editane dtiaun edain edinaudeni adten,.fegn.,en fjpnbrn.,be sn,j.fsne ,.fs.,n </Paper>
                 </Grid>
-                <Grid item md={12}>
-                    <Paper className={classes.paper}>item</Paper>
+                <Grid item >
+                    <Paper className={classes.paper}>item with siginuiet ndiaten idane ditan editane dtiaun edain edinaudeni adten,.fegn.,en fjpnbrn.,be sn,j.fsne ,.fs.,n </Paper>
                 </Grid>
-                <Grid item md={12}>
-                    <Paper className={classes.paper}>item</Paper>
+                <Grid item >
+                    <Paper className={classes.paper}>item with siginuiet ndiaten idane ditan editane dtiaun edain edinaudeni adten,.fegn.,en fjpnbrn.,be sn,j.fsne ,.fs.,n </Paper>
+                </Grid>
+                <Grid item>
+                    <TextField required id="groupName" label="LB-Gruppenname" defaultValue="Stufe X" className={classes.form}/>
                 </Grid>
             </Grid>
 
             <Grid item md={6}>
                     <Typography>
-                        <Box>
+                        <Box className={classes.spaltenTitel}>
                             Stundenplan
                         </Box>
                     </Typography>
             </Grid>
 
-            <Grid item container spacing={0} direction="column" md={3}>
-                <Grid item md={12}>
+            <Grid item container spacing={0} direction="column" justify="flex-start" md={3} align-items="stretch">
+                <Grid item>
                     <Typography>
-                        <Box>
+                        <Box className={classes.spaltenTitel}>
                             Lernbüros
                         </Box>
                     </Typography>
                 </Grid>
-                <Grid item md={12}>
+                <Grid item>
                     <Paper className={classes.paper}>item with siginuiet ndiaten idane ditan editane dtiaun edain edinaudeni adten,.fegn.,en fjpnbrn.,be sn,j.fsne ,.fs.,n </Paper>
                 </Grid>
-                <Grid item md={12}>
+                <Grid item>
                     <Paper className={classes.paper}>item</Paper>
                 </Grid>
-                <Grid item md={12}>
+                <Grid item>
                     <Paper className={classes.paper}>item</Paper>
                 </Grid>
             </Grid>
