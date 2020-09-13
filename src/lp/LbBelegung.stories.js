c0c1505d9a76b778093ab265dd97653787807261
oliver <oliver.burkhard@gmail.com>
2020-09-13 08:54:07 +0200

    Delete Stories

--- src/lp/LbBelegung.stories.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import { LbBelegung } from "./LbBelegung";
-
-export default { title: "LbBelegung"}
-
-export const example = () => <LbBelegung lb={{name: "Math", ort: "Spielwiese", kw: 1, id:1, current: 5, soft: 20, hard: 25, start: 1590311476000}} sus={[]}/>
